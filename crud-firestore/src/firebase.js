cc9ba8c8f70231432fafc9c736da97bfe955ecd7
patxi <patxiarana05@gmail.com>
2023-03-04 12:58:27 -0300

    conecting and installing firebase

---
+++ crud-firestore/src/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyANjvSnUCLo_kuajv6PO2IiUB8pW0VxIKA',
+	authDomain: 'fb-crud-746ac.firebaseapp.com',
+	projectId: 'fb-crud-746ac',
+	storageBucket: 'fb-crud-746ac.appspot.com',
+	messagingSenderId: '355099289737',
+	appId: '1:355099289737:web:a126d5e7e1c01fa21ffe58',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
