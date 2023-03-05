7a2c870f3f9d352255404891b30deb0f9015bcbf
patxi <patxiarana05@gmail.com>
2023-03-05 07:33:05 -0300

    stailing form

--- crud-firestore/src/components/LinkForm.jsx
+++ crud-firestore/src/components/LinkForm.jsx
@@ -2,13 +2,19 @@
 import React from 'react';
 
 export const LinkForm = () => {
-	return(
-    <form className='card card-body'>
-   
-
-
-
-
-    </form>
-    )
+	return (
+		<form className='card card-body'>
+			<div className='form-group input-group'>
+				<div className='input-group-text bg-light'>
+					<i className='material-icons'>instert_link</i>
+				</div>
+				<input
+					type='text'
+					className='form-control'
+					placeholder='url'
+					name='url'
+				/>
+			</div>
+		</form>
+	);
 };
