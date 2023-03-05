db69d04f6027591c97fc3b807b16fd57fa95681f
patxi <patxiarana05@gmail.com>
2023-03-05 10:02:50 -0300

    functions form

--- crud-firestore/src/components/LinkForm.jsx
+++ crud-firestore/src/components/LinkForm.jsx
@@ -1,9 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LinkForm = () => {
+	const [values, setValues] = useState({
+		url: '',
+		name: '',
+		description: ''
+	});
+
+const handleInputChange = (e) => { 
+    e.preventDefault()
+    const {name , value} = e.target ;
+    setValues({...values, [name]:value})
+}
+
+	const handlesubmit = (e) => {
+		e.preventDefault();
+		console.log(values);
+	};
+
 	return (
-		<form className='card card-body'>
+		<form className='card card-body' onSubmit={handlesubmit}>
 			<div className='form-group input-group'>
 				<div className='input-group-text bg-light'>
 					<i className='material-icons'>instert_link</i>
@@ -13,6 +30,7 @@ export const LinkForm = () => {
 					className='form-control'
 					placeholder='url'
 					name='url'
+                    onChange={handleInputChange}
 				/>
 			</div>
 			<div className='form-group input-group'>
@@ -24,6 +42,7 @@ export const LinkForm = () => {
 					className='form-control'
 					name='name'
 					placeholder='Website name'
+                    onChange={handleInputChange}
 				/>
 			</div>
 			<div className='form-group'>
@@ -32,11 +51,10 @@ export const LinkForm = () => {
 					rows='3'
 					className='form-control'
 					placeholder='write a description'
+                    onChange={handleInputChange}
 				/>
 			</div>
-            <button className='btn btn-primary btn-block'>
-                Save
-            </button>
+			<button className='btn btn-primary btn-block'>Save</button>
 		</form>
 	);
 };
