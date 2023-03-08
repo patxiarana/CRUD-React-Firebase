b519dd89c23e6d99df4b69c954e1b81f54bd39c6
patxi <patxiarana05@gmail.com>
2023-03-08 18:24:27 -0300

    fixed errors

--- crud-firestore/src/components/Links.jsx
+++ crud-firestore/src/components/Links.jsx
@@ -1,48 +1,49 @@
 // eslint-disable-next-line no-unused-vars
-import React , {useEffect, useState}from 'react';
+import React, { useEffect, useState } from 'react';
 import { LinkForm } from './LinkForm';
 import { db } from '../firebase';
-import { collection, addDoc,query, onSnapshot } from "firebase/firestore";
-
+import { collection, addDoc, query, onSnapshot } from 'firebase/firestore';
 
 export const Links = () => {
-  const [links, setlinks] = useState()
+	const [links, setlinks] = useState();
 
 	const addOrEdit = async (linkObject) => {
-    try{
-		await addDoc(collection(db, "links"),{linkObject})
-		console.log('new task added');
-  }catch(err){
-    console.error("Error adding document: ", err);
-  }
+		try {
+			await addDoc(collection(db, 'links'), { linkObject });
+			console.log('new task added');
+		} catch (err) {
+			console.error('Error adding document: ', err);
+		}
 	};
- 
-  const getLinks = async() => {
-    try{
-      
-const q = query(collection(db, "links"));
-  onSnapshot(q, (querySnapshot) => {
-  const Website = [];
-  querySnapshot.forEach((doc) => {
-    Website.push({...doc.data(), id:doc.id});
-  });
-  console.log("url websites ", Website);
-});
 
-    }catch(err) {
-      console.error("Error adding document: ", err);
-    }
-  }
+	const getLinks = async () => {
+		try {
+			const q = query(collection(db, 'links'));
+			onSnapshot(q, (querySnapshot) => {
+				const Website = [];
+				querySnapshot.forEach((doc) => {
+					Website.push({ ...doc.data(), id: doc.id });
+				});
+				console.log('url websites ', Website);
+				setlinks(Website);
+			});
+		} catch (err) {
+			console.error('Error adding document: ', err);
+		}
+	};
 
-
-  useEffect( () => {
-    getLinks()
-  },[])
+	useEffect(() => {
+		getLinks();
+	}, []);
 
 	return (
 		<div className=''>
 			<LinkForm addOrEdit={addOrEdit} />
-			Links
+			<div className='col-md-8'>
+				{links.map((link) => {
+				return <h1>{link.name}</h1>;
+				})}
+			</div>
 		</div>
 	);
 };
